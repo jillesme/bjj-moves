bfb33457806027ce21d537de88af86b44ba2781f
Michael Smyth <xanthonius@gmail.com>
2017-05-12 00:07:56 +0100

    i ferget 2 pad

--- src/IndexView.js
+++ src/IndexView.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IndexView = () => {
   return (
-    <div>
+    <div style={{padding: '20px'}}>
       <h1>Welcome, </h1>
       <p>This is a work-in-progress BJJ index app. It aims to index all Brazilian Jiu Jitsu moves.</p>
       <p>It's written using React and manages state with Redux</p>
